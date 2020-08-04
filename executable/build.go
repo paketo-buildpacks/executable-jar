cee811bd46edc85252f276de1981e407d12bd99d
Ben Hale <bhale@vmware.com>
2020-08-04 14:37:29 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- executable/build.go
+++ executable/build.go
@@ -44,6 +44,11 @@ func (b Build) Build(context libcnb.BuildContext) (libcnb.BuildResult, error) {
 	b.Logger.Title(context.Buildpack)
 	result := libcnb.NewBuildResult()
 
+	_, err = libpak.NewConfigurationResolver(context.Buildpack, &b.Logger)
+	if err != nil {
+		return libcnb.BuildResult{}, fmt.Errorf("unable to create configuration resolver\n%w", err)
+	}
+
 	pr := libpak.PlanEntryResolver{Plan: context.Plan}
 
 	ni := false
