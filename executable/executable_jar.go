fcace092b8a1796eb78620c45e3b438e44bd3c56
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-02-16 23:02:00 -0500

    appcds poc * after the executable jar is found, explode it. * It could be a fix to spring-boot#223 - or not

--- executable/executable_jar.go
+++ executable/executable_jar.go
@@ -17,11 +17,15 @@
 package executable
 
 import (
+	"archive/zip"
 	"errors"
 	"fmt"
+	"github.com/paketo-buildpacks/libpak/sherpa"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/magiconair/properties"
 	"github.com/paketo-buildpacks/libjvm"
@@ -57,6 +61,12 @@ func LoadExecutableJAR(appPath string, executableJarGlob string) (ExecutableJAR,
 		if err != nil {
 			return ExecutableJAR{}, fmt.Errorf("unable to parse manifest\n%w", err)
 		}
+		// let's explode the executable jar we found, and remove all other jars
+		tempExplodedJar := os.TempDir() + "/" + fmt.Sprint(time.Now().UnixMilli()) + "/"
+		Unzip(jarPath, tempExplodedJar)
+		os.RemoveAll(appPath)
+		sherpa.CopyDir(tempExplodedJar, appPath)
+		jarPath = appPath
 	}
 
 	if mc, ok := props.Get("Main-Class"); ok {
@@ -126,4 +136,65 @@ func findExecutableJAR(appPath, executableJarGlob string) (string, *properties.P
 	}
 
 	return jarPath, props, nil
+}
+
+func Unzip(src, dest string) error {
+	dest = filepath.Clean(dest) + "/"
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer CloseOrPanic(r)()
+
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		path := filepath.Join(dest, f.Name)
+		// Check for ZipSlip: https://snyk.io/research/zip-slip-vulnerability
+		if !strings.HasPrefix(path, dest) {
+			return fmt.Errorf("%s: illegal file path", path)
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer CloseOrPanic(rc)()
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer CloseOrPanic(f)()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func CloseOrPanic(f io.Closer) func() {
+	return func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}
 }
